58241d372d476c875553fa7f00cba2a36bcccd46
Nyy00 <donylks111@gmail.com>
2025-01-11 19:55:13 +0700

    add history

--- src/View/MenuBar.java
+++ src/View/MenuBar.java
@@ -6,25 +6,28 @@ import java.awt.event.ActionListener;
 public class MenuBar {
     public JMenuBar createMenuBar(ActionListener dashboardListener, ActionListener requestListener,
                                   ActionListener manageUsersListener, ActionListener manageCouriersListener,
-                                  ActionListener exitListener) {
+                                  ActionListener historyListener, ActionListener exitListener) {
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Menu");
         JMenuItem menuDashboard = new JMenuItem("Dashboard");
         JMenuItem menuRequest = new JMenuItem("Request");
         JMenuItem menuManageUsers = new JMenuItem("Pendaftaran Masyarakat");
         JMenuItem menuManageCouriers = new JMenuItem("Pendaftaran Kurir");
+        JMenuItem menuHistory = new JMenuItem("History");
         JMenuItem menuExit = new JMenuItem("Exit");
 
         menuDashboard.addActionListener(dashboardListener);
         menuRequest.addActionListener(requestListener);
         menuManageUsers.addActionListener(manageUsersListener);
         menuManageCouriers.addActionListener(manageCouriersListener);
+        menuHistory.addActionListener(historyListener);
         menuExit.addActionListener(exitListener);
 
         menu.add(menuDashboard);
         menu.add(menuRequest);
         menu.add(menuManageUsers);
         menu.add(menuManageCouriers);
+        menu.add(menuHistory);
         menu.addSeparator();
         menu.add(menuExit);
 
